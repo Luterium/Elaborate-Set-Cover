75e0017bd1f48cde75ba58723f3d9f9b67a5f797
Luterium <nicholaslopes4@gmail.com>
2018-11-21 17:21:18 -0200

    Melhorada a cobertura de elementos
    Criada uma nova função para a cobertura de elementos de complexidade assintótica menor que a anterior. Também foram novas variáveis na função para facilitar a compreensão da mesma.

--- elaborateSetCover/tratainput.cpp
+++ elaborateSetCover/tratainput.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <math.h>
 #include "subconjunto.h"
+#include <queue>
 #define EPS 0.001
 
 using namespace std;
@@ -19,26 +20,33 @@ void DesceHeap(vector <Subset>& Subsets, int i, vector <int>& subsetsIndexes){
     int menor;
     // Laço de repetição
     leftChild  = 2*i+1;
-    rightChild = 2*i+2;
+    while(leftChild < Subsets.size()){
+        leftChild  = 2*i+1;
+        rightChild = 2*i+2;
 
-    if((rightChild < Subsets.size()) && ((Subsets[i].returnCurrentWeight() > Subsets[rightChild].returnCurrentWeight()+EPS) ||
-                                         (comparaFloats(Subsets[i].returnCurrentWeight(),Subsets[rightChild].returnCurrentWeight())
-                                          && Subsets[i].getCurrentSize() < Subsets[rightChild].getCurrentSize())))
-        menor = rightChild;
-    else menor = i;
-    if((leftChild < Subsets.size()) && ((Subsets[menor].returnCurrentWeight() > Subsets[leftChild].returnCurrentWeight()+EPS) ||
-                                        (comparaFloats(Subsets[menor].returnCurrentWeight(),Subsets[leftChild].returnCurrentWeight())
-                                         && Subsets[menor].getCurrentSize() < Subsets[leftChild].getCurrentSize())))
-        menor = leftChild;
+        if((rightChild < Subsets.size()) && ((Subsets[i].returnCurrentWeight() > Subsets[rightChild].returnCurrentWeight()+EPS) ||
+                                             (comparaFloats(Subsets[i].returnCurrentWeight(),Subsets[rightChild].returnCurrentWeight())
+                                              && Subsets[i].getCurrentSize() < Subsets[rightChild].getCurrentSize())))
+             menor = rightChild;
+        else menor = i;
 
-    if (menor != i){
-        aux = Subsets[menor];
-        Subsets[menor] = Subsets[i];
-        Subsets[i] = aux;
+        if((leftChild < Subsets.size()) && ((Subsets[menor].returnCurrentWeight() > Subsets[leftChild].returnCurrentWeight()+EPS) ||
+                                            (comparaFloats(Subsets[menor].returnCurrentWeight(),Subsets[leftChild].returnCurrentWeight())
+                                             && Subsets[menor].getCurrentSize() < Subsets[leftChild].getCurrentSize())))
+            menor = leftChild;
 
-        subsetsIndexes[Subsets[i].getSubsetNumber()]     = i;
-        subsetsIndexes[Subsets[menor].getSubsetNumber()] = menor;
-        DesceHeap(Subsets, menor, subsetsIndexes);
+        if (menor == i){
+            break;
+        }
+        else{
+            aux = Subsets[menor];
+            Subsets[menor] = Subsets[i];
+            Subsets[i] = aux;
+
+            subsetsIndexes[Subsets[i].getSubsetNumber()]     = i;
+            subsetsIndexes[Subsets[menor].getSubsetNumber()] = menor;
+            i = menor;
+        }
     }
     return;
 }
@@ -46,6 +54,34 @@ void DesceHeap(vector <Subset>& Subsets, int i, vector <int>& subsetsIndexes){
 void Heapify(vector <Subset>& Subsets, vector <int>& subsetsIndexes){
     for(int i = floor(Subsets.size())/2; i >= 0; i--){
         DesceHeap(Subsets, i, subsetsIndexes);
+    }
+}
+
+void betterCoverElements(vector <Subset>& Subsets, vector <int>& subsetsIndexes, vector <bool>& elementosCobertos, int& elementosDescobertosSize, vector < vector <int> >& elementos){
+    int cZI = Subsets[0].getSubsetNumber();
+    vector<bool> changedSubsets(Subsets.size(), false);
+    priority_queue<int> changedSubsetsQueue;
+
+    for(unsigned int i = 0; i < Subsets[subsetsIndexes[cZI]].getElementos().size(); i++){
+        int y = Subsets[subsetsIndexes[cZI]].getElementos().at(i);
+        if(!elementosCobertos[y]){
+            elementosCobertos[y] = true;
+            elementosDescobertosSize--;
+            for(unsigned int j = 0; j < elementos[y].size() ; j++){
+                int z = subsetsIndexes[elementos[y].at(j)];
+                Subsets[z].decrementSize();
+                if(!changedSubsets[z]){
+                    changedSubsets[z] = true;
+                	changedSubsetsQueue.push(z);
+                }
+            }
+        }
+    }
+
+    while(!changedSubsetsQueue.empty()){
+    	int auxPos = changedSubsetsQueue.top();
+    	changedSubsetsQueue.pop();
+    	DesceHeap(Subsets, auxPos, subsetsIndexes);
     }
 }
 
@@ -98,10 +134,9 @@ void trataInput(){
     }
     Heapify(Subsets, subsetsIndexes);
     while(elementosDescobertosSize){
-        cout << "w chapeu: " << Subsets[0].returnCurrentWeight() << endl;
         totalWeight += Subsets[0].getWeight();
         subconjuntosEscolhidos.push_back(Subsets[0].getSubsetNumber());
-        coverElements(Subsets, subsetsIndexes, elementosCobertos, elementosDescobertosSize, elementos);
+        betterCoverElements(Subsets, subsetsIndexes, elementosCobertos, elementosDescobertosSize, elementos);
     }
     cout << "Total cover cost: " << totalWeight << endl << endl << "Subconjuntos escolhidos: ";
     for(unsigned int i = 0; i < subconjuntosEscolhidos.size(); i++){
