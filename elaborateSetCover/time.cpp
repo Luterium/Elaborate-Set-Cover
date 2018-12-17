234d393d32844eaf3251c9c52a5986021fe7ff33
Nicholas Lopes <nicholaslopes4@gmail.com>
2018-12-17 11:28:28 -0200

    Primeiros timelogs, mede tempo melhor
    Timelogs para o algoritmo com desempenho teórico pior, bem como uma medição de tempo mais bem feita

--- elaborateSetCover/time.cpp
+++ elaborateSetCover/time.cpp
@@ -9,22 +9,41 @@ int main(int argc, char *argv[]){
 	std::string auxInputName;
 	ifstream inputName;
 	inputName.open("testCases.txt");
+	fstream timeLog;
+	timeLog.open("timeLogElaborate.txt", std::fstream::in | std::fstream::out | std::fstream::app);
 	double timeSpent;
 	struct timespec start, finish;
+	double minTime = 1000;
+	double maxTime = 0;
+	double avgTime;
 
 	while(!inputName.eof()){
-		timeSpent = 0;
 		std::stringstream stringBuilder;
 		inputName >> auxInputName;
 		stringBuilder << "main.exe setCoverTestCases/" << auxInputName;
 		std::string completeString = stringBuilder.str();
 		cout << endl << auxInputName << endl;
-		clock_gettime(CLOCK_REALTIME, &start);
-		int retCode = system(completeString.c_str());
-		clock_gettime(CLOCK_REALTIME, &finish);
-		timeSpent =  (finish.tv_sec - start.tv_sec);
-		timeSpent += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
-		cout << "Time spent: " << timeSpent << " seconds" << endl;
+		timeLog << auxInputName << endl;
+		avgTime = 0;
+		minTime = 1000;
+		maxTime = 0;
+
+		for(int i = 0; i < 25; i++){
+			clock_gettime(CLOCK_REALTIME, &start);
+			int retCode = system(completeString.c_str());
+			clock_gettime(CLOCK_REALTIME, &finish);
+			timeSpent =  (finish.tv_sec - start.tv_sec);
+			timeSpent += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+			cout << "Time spent: " << timeSpent << " seconds" << endl;
+
+			if(timeSpent > maxTime)
+				maxTime = timeSpent;
+			else if(timeSpent < minTime)
+				minTime = timeSpent;
+			timeLog << i << ": " << timeSpent << endl;
+			avgTime += timeSpent;
+		}
+		timeLog << "Min: " << minTime << endl << "Max:" << maxTime << endl << "Avg:" << avgTime/25 << endl << endl;
 	}
 	return 0;
 }
